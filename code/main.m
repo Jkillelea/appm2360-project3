8cce6f8c08c073e4ad97d8767f21aed89e22b7e2
Jacob Killelea <Jkillelea@protonmail.ch>
2017-04-17 15:47:47 -0600

    plot direction field

--- code/main.m
+++ code/main.m
@@ -4,26 +4,34 @@ clear; clc; close all;
 alpha = 0.00001;
 beta  = 0.00003;
 gamma = 0.00001;
-N0    = 5000;
+N0    = 50000;
 
 % SZR Model
 dS = @(S, Z) -beta.*Z.*S;
-dZ = @(S, Z) beta.*Z.*S + gamma.*(N0 - S - Z) - alpha.*Z.*S;
+dZ = @(S, Z) (beta - alpha).*Z.*S + gamma.*(N0 - S - Z);
 
-data_ds = ones(N0);
-data_dz = ones(N0);
+[S, Z] = meshgrid(0:(N0/100):N0, 0:(N0/100):N0);
 
-for S = 1:length(N0)
-  for Z = 1:length(N0)
-    data_ds(S, Z) = dS(S, Z);
-    data_dz(S, Z) = dZ(S, Z);
-  end
-end
+data_ds = dS(S, Z);
+data_dz = dZ(S, Z);
 
+fig = figure; hold on;
 
+quiver(S, Z, data_ds, data_dz);
+title('Direction Field of Humans vs Zombies');
+xlabel('Humans');
+ylabel('Zombies');
+print(fig, '-dpng', 'direction_field');
 
-% tspan  = [0 35]; % days
-% y0     = [49999; 1];
-% [t, y] = ode45(@(t, y) szr(t, y, alpha, beta, gamma, N0), tspan, y0);
-%
-% plot(t, y);
+close(fig);
+
+fig = figure; hold on;
+tspan  = [0 35]; % days
+y0     = [49999; 1];
+[t, y] = ode45(@(t, y) szr(t, y, alpha, beta, gamma, N0), tspan, y0);
+plot(t, y(:, 1));
+plot(t, y(:, 2));
+legend('Human population', 'Zombie population');
+title('Human vs Zombie Poulations');
+xlabel('Days');
+ylabel('Number of individuals');
